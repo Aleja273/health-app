bf3fb7fceb05c4e1ea2b1fbe73627a1a51d64bf8
Alejandra <alejandratobar2102@gmail.com>
2025-04-22 21:30:32 -0500

    Restore Project

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,14 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-<<<<<<< HEAD
- 
- const SUPABASE_URL = "https://wredipjwrhyegenhxoog.supabase.co";
- const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyZWRpcGp3cmh5ZWdlbmh4b29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NjIwNTUsImV4cCI6MjA1OTEzODA1NX0.Xk8dWgGZAjK7DeE5SE4nhwy5keCB3lOh5OUF9F6q9Xc";
- 
- export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-=======
 
-const SUPABASE_URL = "https://wredipjwrhyegenhxoog.supabase.co";
-const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyZWRpcGp3cmh5ZWdlbmh4b29nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NjIwNTUsImV4cCI6MjA1OTEzODA1NX0.Xk8dWgGZAjK7DeE5SE4nhwy5keCB3lOh5OUF9F6q9Xc";
+const SUPABASE_URL ="https://vrryjqqjlahuisfbsziy.supabase.co";
+const SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZycnlqcXFqbGFodWlzZmJzeml5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1NjIwNzksImV4cCI6MjA1OTEzODA3OX0.tpQvGa91Jm-VRn6J-BOov_0Q3L5WN1l5bKoz8oEcXc0";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
->>>>>>> fbfeab7dfd93bae5a07dd0a3ad3c0913cdc2f84f
+  
